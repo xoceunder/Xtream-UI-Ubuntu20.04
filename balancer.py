5c0fe23d62bcf2446af8ebd23fddc682ebee8ccb
xoceunder <xoceunder@gmail.com>
2024-09-24 18:16:21 -0400

    Update balancer.py

--- balancer.py
+++ balancer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import subprocess, os, random, string, sys, shutil, socket, zipfile, urllib.request, urllib.error, urllib.parse, json, base64
 from itertools import cycle
