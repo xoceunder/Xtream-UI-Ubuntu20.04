8bd4e5ad072b68f96719e961c77faea34b9bcee8
xoceunder <xoceunder@gmail.com>
2024-10-03 18:23:06 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -106,9 +106,10 @@ def prepare(rType="MAIN"):
         printc("Installing %s" % rPackage)
         os.system("echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections > /dev/null")
         os.system("echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections > /dev/null")
-        os.system("sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install %s" % rPackage)
+        os.system("sudo DEBIAN_FRONTEND=noninteractive apt-get -yq install %s > /dev/null" % rPackage)
     printc("Installing pip3")
     os.system("add-apt-repository universe > /dev/null 2>&1 && curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py > /dev/null 2>&1 && python3 get-pip.py > /dev/null 2>&1")
+    os.system("add-apt-repository universe > /dev/null 2>&1 && curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py > /dev/null 2>&1 && python2 get-pip.py > /dev/null 2>&1 && pip2 install paramiko > /dev/null 2>&1")
     printc("Installing pip modules")
     os.system("pip3 install ndg-httpsclient > /dev/null 2>&1 && pip3 install pyopenssl > /dev/null 2>&1 && pip3 install pyasn1 > /dev/null 2>&1")
     os.system("apt-get install -f > /dev/null") # Clean up above
